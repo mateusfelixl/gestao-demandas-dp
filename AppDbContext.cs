0ab6a35794f106c72ae78ac9f3b6ceb429e3834f
Mateus Felix <mateus.flima212@gmail.com>
2026-04-13 16:03:38 -0300

    feat: configurar Entity Framework e criar classes iniciais

---
+++ AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DpTaskManager.Api.Models;
+using System;
+
+namespace DpTaskManager.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
---
+++ Tarefa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DpTaskManager.Api.Models
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; } = string.Empty;
+
+        public string Descricao { get; set; } = string.Empty;
+
+        public string Status { get; set; } = "Pendente";
+
+        public DateTime DataCriacao { get; set; } = DateTime.Now;
+    }
+}
